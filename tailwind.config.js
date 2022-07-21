8533f5e42095a47e9585d1e0421439ec266e2996
justEstif <95101485+justEstif@users.noreply.github.com>
2022-07-21 12:51:56 -0400

    completed mobile layout with small images

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,4 +5,11 @@ module.exports = {
     './src/**/*.{html,js}',
     './index.html',
   ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'split-white-cyan': "linear-gradient(140deg, #06b6d4 60% , white 40%);"
+      }
+    }
+  }
 }
