9c0b76b3cf5a73bb0e9c15620e175e0063a4b9ac
Estifanos <estifanosbeyene21@gmail.com>
2022-11-01 13:44:53 -0400

    feat: add separate motion component

---
+++ app/(components)/header.tsx
@@ -0,0 +1,75 @@
+import motion from './motion'
+import { SocialIcon } from "react-social-icons";
+
+type Props = {};
+
+const Header = ({}: Props) => {
+  return (
+    <header className="flex sticky top-0 z-20 justify-between items-start p-5 mx-auto max-w-7xl xl:items-center">
+      {/* TODO: Add transition to the navbar */}
+
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex flex-row items-center transition ease-in-out delay-150"
+      >
+        <SocialIcon
+          url="https://youtube.com/jaketrent"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+
+        <SocialIcon
+          url="https://youtube.com/jaketrent"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://youtube.com/jaketrent"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 1,
+        }}
+        className="flex flex-row items-center text-green-300 cursor-pointer"
+      >
+        <SocialIcon
+          // TODO: Remove the cursor-pointer
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="hidden text-sm text-gray-400 uppercase md:inline-flex">
+          Get In Touch
+        </p>
+      </motion.div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Portfolio</title>
+        <meta name="description" content="Estifanos's Portfolio" />
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
