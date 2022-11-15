cadadd6f56c1ab27c66611861a755a533e7a66fc
Estifanos <estifanosbeyene21@gmail.com>
2022-11-15 15:19:57 -0500

    feat: extract dark mode toggle button

--- app/(components)/navBar.tsx
+++ app/(components)/navBar.tsx
@@ -1,17 +1,11 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
+import React from "react";
+import DarkModeToggle from "./darkModeToggle";
 import MobileMenu from "./mobileMenu";
 import NavItem from "./navItem";
 
 type Props = {};
 
 function NavBar({}: Props) {
-  const [mounted, setMounted] = useState(false);
-  const { resolvedTheme, setTheme } = useTheme();
-
-  // After mounting, we have access to the theme
-  useEffect(() => setMounted(true), []);
   return (
     <div className="flex flex-col justify-center px-8">
       <nav className="flex relative justify-between items-center pt-8 pb-8 mx-auto w-full max-w-2xl text-gray-900 bg-gray-50 bg-opacity-60 border-gray-200 sm:pb-16 dark:text-gray-100 dark:bg-gray-900 dark:border-gray-700">
@@ -26,38 +20,7 @@ function NavBar({}: Props) {
           <NavItem href="/blog" text="Blog" />
           <NavItem href="/snippets" text="Snippets" />
         </div>
-        <button
-          aria-label="Toggle Dark Mode"
-          type="button"
-          className="flex justify-center items-center w-9 h-9 bg-gray-200 rounded-lg ring-gray-300 transition-all dark:bg-gray-600 hover:ring-2"
-          onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
-        >
-          {mounted && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              className="w-5 h-5 text-gray-800 dark:text-gray-200"
-            >
-              {resolvedTheme === "dark" ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
-              )}
-            </svg>
-          )}
-        </button>
+        <DarkModeToggle />
       </nav>
     </div>
   );
