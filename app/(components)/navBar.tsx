9de51fbd5852ce5a1341ce4a9ce455fb642d5552
Estifanos <estifanosbeyene21@gmail.com>
2022-11-16 11:31:45 -0500

    refactor: move provider

--- app/(components)/navBar.tsx
+++ app/(components)/navBar.tsx
@@ -8,13 +8,15 @@ type Props = {};
 function NavBar({}: Props) {
   return (
     <div className="flex flex-col justify-center px-8">
-      <nav className="flex relative justify-between items-center pt-8 pb-8 mx-auto w-full max-w-2xl text-gray-900 bg-gray-50 bg-opacity-60 border-gray-200 sm:pb-16 dark:text-gray-100 dark:bg-gray-900 dark:border-gray-700">
+      <nav className="flex relative justify-between items-center py-6 mx-auto w-full max-w-2xl text-gray-900 bg-gray-50 bg-opacity-60 border-gray-200 sm:pb-16 dark:text-gray-100 dark:bg-gray-900 dark:border-gray-700">
         <div className="ml-[-0.60rem]">
           <MobileMenu />
           <NavItem href="/" text="Home" />
           <NavItem href="/projects" text="Projects" />
           <NavItem href="/blog" text="Blog" />
+          <NavItem href="/about" text="About" />
         </div>
+
         <DarkModeToggle />
       </nav>
     </div>
--- app/(components)/themeProvider.tsx
+++ app/(components)/themeProvider.tsx
@@ -3,6 +3,8 @@
 
 import { ThemeProvider as NextThemeProvider } from "next-themes";
 
-export function ThemeProvider({ children }: { children: React.ReactNode }) {
+function ThemeProvider({ children }: { children: React.ReactNode }) {
   return <NextThemeProvider attribute="class">{children}</NextThemeProvider>;
 }
+
+export default ThemeProvider
