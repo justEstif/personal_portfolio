46ffa24b8150d471c66a3a1d7d26ac52177d3214
Estifanos <estifanosbeyene21@gmail.com>
2022-11-16 14:43:24 -0500

    feat: add placeholder avatar image

--- app/(components)/navBar.tsx
+++ app/(components)/navBar.tsx
@@ -8,7 +8,7 @@ type Props = {};
 function NavBar({}: Props) {
   return (
     <div className="flex flex-col justify-center px-8">
-      <nav className="flex relative justify-between items-center py-6 mx-auto w-full max-w-2xl text-gray-900 bg-gray-50 bg-opacity-60 border-gray-200 sm:pb-16 dark:text-gray-100 dark:bg-gray-900 dark:border-gray-700">
+      <nav className="flex relative justify-between items-center py-8 mx-auto w-full max-w-2xl text-gray-900 bg-gray-50 bg-opacity-60 border-gray-200 sm:pb-16 dark:text-gray-100 dark:bg-gray-900 dark:border-gray-700">
         <div className="ml-[-0.60rem]">
           <MobileMenu />
           <NavItem href="/" text="Home" />
---
+++ app/portfolio.ts
@@ -0,0 +1,6 @@
+export const greeting = {
+  username: "Estifanos Beyene",
+  title:
+    "A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks.",
+  subtitle: "Looking for full time work as Software Developer",
+};
