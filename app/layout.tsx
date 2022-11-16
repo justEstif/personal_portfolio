d38b6f22795a9b46b2bf036d7e5fbfbf5faf8e2d
Estifanos <estifanosbeyene21@gmail.com>
2022-11-16 11:23:08 -0500

    feat: add font awesome icons, remove social icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,31 @@
 import React from "react";
 import cn from "classnames";
+
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
 import "styles/globals.css"; // tailwind
 import Footer from "./(components)/footer";
 import NavBar from "./(components)/navBar";
 import { Inter } from "@next/font/google";
-import { ThemeProvider } from "./providers";
+import ThemeProvider from "./(components)/themeProvider";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const interVariable = Inter();
+config.autoAddCss = false;
 
 function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className="bg-gray-50 dark:bg-gray-900">
+      <body
+        className={cn("bg-gray-50 dark:bg-gray-900", interVariable.className)}
+      >
         <ThemeProvider>
           <NavBar />
-          <main
-            className={cn(
-              "flex flex-col justify-center px-8 bg-gray-50 dark:bg-gray-900",
-              interVariable.className
-            )}
-          >
+          <main className="flex flex-col justify-center px-8 bg-gray-50 dark:bg-gray-900">
             {children}
             <Footer />
           </main>
