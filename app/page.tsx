fb15d86684d163b521270b7d0b36c3d2e407c8da
Estifanos <estifanosbeyene21@gmail.com>
2022-11-16 19:32:13 -0500

    feat: add portfolio details as prop

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,38 @@
 import React from "react";
 import Image from "next/image";
+import { TGreetings, greeting } from "./portfolio";
 
 type Props = {};
 
 function Page({}: Props) {
   return (
-    <div className="flex flex-col justify-center items-start pb-16 mx-auto max-w-2xl border-gray-200 dark:border-gray-700">
-      <Greetings />
+    <div className="flex flex-col justify-center items-start mx-auto max-w-2xl border-gray-200 dark:border-gray-700">
+      <Greetings {...greeting} />
     </div>
   );
 }
 export default Page;
 
-function Greetings() {
+function Greetings(props: TGreetings) {
   return (
-    <div className="flex flex-col-reverse items-start w-full sm:flex-row">
-      <div className="flex flex-col flex-grow pr-8">
+    <div className="flex flex-col-reverse justify-between items-start w-full sm:flex-row">
+      <div className="flex flex-col gap-4 w-full max-w-md">
         <h1 className="mb-1 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
-          Name
+          {props.name || "Name"}
         </h1>
-        <h2 className="mb-4 text-gray-700 dark:text-gray-200">Title</h2>
-        <p className="mb-16 text-gray-600 dark:text-gray-400">Subtitle</p>
+        <h2 className="mb-4 text-gray-700 dark:text-gray-200">
+          {props.title || "Title"}
+        </h2>
+        <p className="mb-16 text-gray-600 dark:text-gray-400">
+          {props.subtitle || "Subtitle"}
+        </p>
       </div>
-      <div className="relative mr-auto mb-8 w-36 md:w-44">
+      <div className="relative mb-8">
         <Image
-          alt="Avatar Image"
+          alt={props.img.alt || "Img alt"}
           height={176}
           width={176}
-          src="/avatar.jpg"
+          src={props.img.src || "/avatar.jpg"}
           sizes="30vw"
           priority
           className="rounded-full filter grayscale"
