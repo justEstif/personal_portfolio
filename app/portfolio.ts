ebe43883a06144e88dc7321e559b6d5d85b103a6
Estifanos <estifanosbeyene21@gmail.com>
2022-11-16 19:31:51 -0500

    feat: add portfolio data

--- app/portfolio.ts
+++ app/portfolio.ts
@@ -1,6 +1,20 @@
-export const greeting = {
-  username: "Estifanos Beyene",
+export type TGreetings = {
+  name: string;
+  title: string;
+  subtitle: string;
+  img: {
+    src: string;
+    alt: string;
+  };
+};
+
+export const greeting: TGreetings = {
+  name: "Estifanos Beyene",
   title:
-    "A passionate Full Stack Software Developer 🚀 having an experience of building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks.",
+    "A passionate Full Stack Software Developer building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks.",
   subtitle: "Looking for full time work as Software Developer",
+  img: {
+    src: "/avatar.jpg",
+    alt: "Estifanos Beyene Image",
+  },
 };
