2a82e8e73f9d5bac660438c3e8fbfcf77fa563cd
Estifanos <estifanosbeyene21@gmail.com>
2022-11-17 19:54:44 -0500

    feat: change style of link

--- app/(components)/projectCard.tsx
+++ app/(components)/projectCard.tsx
@@ -8,15 +8,21 @@ function ProjectCard({ slug, title, icon, gradient }: TProjectCard) {
   return (
     <Link
       href={`/projects/${slug}`}
-      className="flex flex-col justify-center items-center p-3 bg-red-300 rounded-xl"
+      className={cn(
+        "flex flex-col gap-3 justify-center items-center p-4 rounded-xl border-4 border-transparent border-solid",
+        gradient
+      )}
     >
-      <FontAwesomeIcon icon={icon} className="w-16 h-16 md:w-24 md:h-24" />
-
-      <h4 className="mb-6 w-full text-lg font-medium tracking-tight text-gray-900 sm:mb-10 md:text-lg dark:text-gray-100">
+      <FontAwesomeIcon icon={projectIcon} className="w-16 h-16" />
+      <h4 className="text-lg font-medium tracking-tight text-gray-900 dark:text-gray-50">
         {title}
       </h4>
-      <p>Used tech here</p>
-      {/* <div className="flex flex-col justify-between items-center p-4 h-full bg-gray-50 rounded-lg dark:bg-gray-900"></div> */}
+        <p>Some project details</p>
+      <div>
+        {techIcons.map((techIcon) => (
+          <div key={nanoid()}>{<FontAwesomeIcon icon={techIcon} />}</div>
+        ))}
+      </div>
     </Link>
   );
 }
