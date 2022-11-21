dab79351efbf8935bcef21c548705c78e832027b
Estifanos <estifanosbeyene21@gmail.com>
2022-11-21 09:36:44 -0500

    feat: improve footer style

--- app/(components)/footer.tsx
+++ app/(components)/footer.tsx
@@ -1,54 +1,29 @@
 import React from "react";
 import Link from "next/link";
 
-import { footer } from "../portfolio";
-
 type Props = {};
 
-function Footer() {
+function Footer({ }: Props) {
   return (
-    <footer className="flex flex-col justify-center items-start mx-auto mb-8 w-full max-w-2xl">
-      <hr className="mb-8 w-full border-gray-200 dark:border-gray-800 border-1" />
-      <div className="grid grid-cols-1 gap-4 pb-16 w-full max-w-2xl sm:grid-cols-3">
-        <div className="flex flex-col space-y-4">
-          <Link
-            href="/"
-            className="text-gray-500 transition hover:text-gray-600"
-          >
-            Home
-          </Link>
-          <Link
-            href="/about"
-            className="text-gray-500 transition hover:text-gray-600"
-          >
-            About
-          </Link>
-        </div>
-        <div className="flex flex-col space-y-4">
-          <Link
-            className="text-gray-500 transition hover:text-gray-600"
-            target="_blank"
-            rel="noopener noreferrer"
-            href={footer.github}
-          >
-            GitHub
-          </Link>
-        </div>
-        <div className="flex flex-col space-y-4">
-          <Link
-            href="/projects"
-            className="text-gray-500 transition hover:text-gray-600"
-          >
-            Projects
-          </Link>
-          <Link
-            href="/blog"
-            className="text-gray-500 transition hover:text-gray-600"
-          >
-            Blog
-          </Link>
-        </div>
-      </div>
+    <footer className="flex gap-8 justify-center items-start my-8 mx-auto w-full max-w-2xl">
+      <Link href="/" className="text-gray-500 transition hover:text-gray-600">
+        Home
+      </Link>
+
+      <Link
+        href="/projects"
+        className="text-gray-500 transition hover:text-gray-600"
+      >
+        Projects
+      </Link>
+      <Link
+        href="/blog"
+        className="text-gray-500 transition hover:text-gray-600"
+      >
+        Blog
+      </Link>
+
+
     </footer>
   );
 }
