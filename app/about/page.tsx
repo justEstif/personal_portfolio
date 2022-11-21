319a2417e3e986344080cc72dc8c0604e3245c8b
Estifanos <estifanosbeyene21@gmail.com>
2022-11-21 15:28:37 -0500

    feat: add basic layout for about page

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+function Page({}: Props) {
+  return (
+    <div className="flex flex-col justify-center items-start mx-auto max-w-2xl border-gray-200 dark:border-gray-700">
+      <h1 className="mb-8 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
+        About Me
+      </h1>
+    </div>
+  );
+}
+
+export default Page;
