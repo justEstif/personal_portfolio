f893720f2d7fc254e7225ca1891d4608a5ac2c85
Estifanos <estifanosbeyene21@gmail.com>
2022-11-21 09:45:48 -0500

    feat: add blogs data

--- app/portfolio.ts
+++ app/portfolio.ts
@@ -1,19 +1,16 @@
-import { TFooter, TGreetings, TProjects } from "lib/types";
+import { TBlogCards, TFooter, TGreetings, TProjects } from "lib/types";
 
 export const greetings: TGreetings = {
   name: "Estifanos Beyene",
+  email: "estifanosbeyene21@gmail.com",
   title:
     "A passionate Full Stack Software Developer building Web applications with JavaScript / Reactjs / Nodejs and some other cool libraries and frameworks.",
-  subtitle: "Looking for full time work as Software Developer",
   img: {
     src: "/avatar.jpg",
     alt: "Estifanos Beyene Image",
   },
 };
 
-export const footer: TFooter = {
-  github: "https://github.com/justEstif",
-};
 export const projects: TProjects = [
   {
     title: "Name of Project",
@@ -24,5 +21,17 @@ export const projects: TProjects = [
     },
     gradient:
       "border-gradient-br-light-purple-gray-50  dark:border-gradient-br-light-purple-gray-900",
+  },
+];
+
+export const footer: TFooter = {
+  github: "https://github.com/justEstif",
+};
+
+export const blogs: TBlogCards = [
+  {
+    title: "Blog Title",
+    slug: "blog-slug",
+    date: new Date("2022-11-19T15:09:59.443Z"),
   },
 ];
