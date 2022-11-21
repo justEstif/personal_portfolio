8804fc4502e9a6d72dc86b557c1bc12b138f6a9f
Estifanos <estifanosbeyene21@gmail.com>
2022-11-21 10:58:35 -0500

    feat: add basic layout to blog and project page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 function Page({}: Props) {
   return (
-    <div>Blog Page (Under Construction)</div>
-  )
+    <div className="flex flex-col justify-center items-start mx-auto max-w-2xl border-gray-200 dark:border-gray-700">
+      <h1 className="mb-1 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
+        Blog
+      </h1>
+    </div>
+  );
 }
 
-export default Page
+export default Page;
--- app/project/page.tsx
+++ app/project/page.tsx
@@ -3,7 +3,13 @@ import React from "react";
 type Props = {};
 
 function Page({}: Props) {
-  return <div>Project Page (Under Construction)</div>;
+  return (
+    <div className="flex flex-col justify-center items-start mx-auto max-w-2xl border-gray-200 dark:border-gray-700">
+      <h1 className="mb-1 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
+        Projects
+      </h1>
+    </div>
+  );
 }
 
 export default Page;
