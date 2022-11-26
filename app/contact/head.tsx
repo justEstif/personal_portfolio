0bb570d0f12d8baf33a9a3786cc1960c89d3a2f4
Estifanos <estifanosbeyene21@gmail.com>
2022-11-26 05:31:07 -0500

    feat: add contact page head

---
+++ app/contact/head.tsx
@@ -0,0 +1,20 @@
+import { head } from "app/data";
+type Props = {};
+
+const Head = ({}: Props) => {
+  const title = "Contact - Estifanos Beyene";
+  return (
+    <>
+      <title>{title}</title>
+      <link rel="shortcut icon" href="/static/favicon.ico" />
+      <meta name="robots" content="follow, index" />
+      <meta content={head.description} name="description" />
+      <meta property="og:type" content={head.type} />
+      <meta property="og:site_name" content="Estifanos Beyene" />
+      <meta property="og:description" content={head.description} />
+      <meta property="og:title" content={title} />
+    </>
+  );
+};
+
+export default Head;
