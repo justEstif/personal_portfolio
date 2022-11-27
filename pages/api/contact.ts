efce08a35ad75a496b28f86a7c72f5522f9853c7
Estifanos <estifanosbeyene21@gmail.com>
2022-11-27 11:20:32 -0500

    fix: change next api request and response type

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,11 +1,7 @@
-import { TFormState } from "lib/types";
-import type { NextResponse, NextRequest } from "next/server";
+import { NextApiRequest, NextApiResponse } from "next";
 import nodemailer from "nodemailer";
 
-type Override<T1, T2> = Omit<T1, keyof T2> & T2;
-type TContactReq = Override<NextRequest, { body: TFormState }>;
-
-const handler = (req: TContactReq, res: NextResponse) => {
+const handler = (req: NextApiRequest, res: NextApiResponse) => {
   const { subject, email, message } = req.body;
 
   const transporter = nodemailer.createTransport({
@@ -25,10 +21,8 @@ const handler = (req: TContactReq, res: NextResponse) => {
 
   transporter.sendMail(mailOptions, (err, info) => {
     if (err) {
-      console.warn(err);
       res.status(400).json(err);
     } else {
-      console.log("Email sent, " + info);
       res.status(200).json(info);
     }
   });
