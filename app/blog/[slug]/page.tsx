cd48e9e8ed59fd2c57dcd47c920c09a6bde01cae
Estifanos <estifanosbeyene21@gmail.com>
2022-11-27 11:31:15 -0500

    feat: add todos

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -5,13 +5,16 @@ import matter from "gray-matter";
 import path from "path";
 
 type Props = {
-  params: { slug: string };
+  params: { slug: string }; // TODO how to pass the title
 };
 
+/**
+ * @description function to get the markdown given the slug
+ */
 const getPostData = async (slug: string) => {
   const fullPath = path.join(process.cwd(), "public", "blog", `${slug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
-  const matterResult = matter(fileContents);
+  const matterResult = matter(fileContents); // TODO create type
   const processedContent = await remark()
     .use(html)
     .process(matterResult.content);
@@ -29,8 +32,15 @@ const Page = async ({ params: { slug } }: Props) => {
       <h1 className="mb-8 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
         This is the blog
       </h1>
+      {/**
+       * TODO: add the minor details
+       */}
+      <div className="flex justify-between items-center my-6 w-full text-sm leading-8">
+        <p>img/TODO Name/date</p>
+        <p>time to read</p>
+      </div>
       <article
-        className="space-y-3"
+        className="space-y-5 text-justify"
         dangerouslySetInnerHTML={{ __html: blog.contentHtml }}
       />
     </div>
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,13 +4,16 @@ import { allBlogCards } from "app/data";
 
 type Props = {};
 
-function Page({}: Props) {
+function Page({ }: Props) {
   return (
     <div className="flex flex-col justify-center items-start mx-auto max-w-2xl border-gray-200 dark:border-gray-700">
       <h1 className="mb-8 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
         Blog
       </h1>
 
+      {/**
+       * TODO go through the public/blog file and loop through the metadata
+       */}
       <div className="w-full">
         {allBlogCards.map((blog) => (
           <BlogCard key={nanoid()} {...blog} />
