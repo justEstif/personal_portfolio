ea0cc95bb5354dbdf71db6aedaef50aa28970b45
Estifanos <estifanosbeyene21@gmail.com>
2022-11-28 10:25:46 -0500

    feat: add photo to blog slug page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import { getDoc } from "lib/remark";
+import { greetings } from "app/data";
 
 type Props = {
   params: { slug: string }; // TODO how to pass the title
@@ -9,17 +11,24 @@ const Page = async ({ params: { slug } }: Props) => {
   return (
     <div className="flex flex-col justify-center items-start mx-auto max-w-2xl border-gray-200 dark:border-gray-700">
       <h1 className="mb-8 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
-        This is the blog
+        {blog.metadata.title}
       </h1>
-      <div className="flex justify-between items-center my-6 w-full text-sm leading-8">
+      <div className="flex justify-between items-center my-4 w-full text-sm leading-8">
         <div className="flex gap-3">
+          <Image
+            src={greetings.img.src}
+            alt={greetings.img.alt}
+            height={30}
+            width={30}
+            className="rounded-full filter grayscale"
+          />
           <p>{blog.metadata.author}</p>
           <p>{blog.metadata.date}</p>
         </div>
         <p>{blog.metadata.readingTime} min read</p>
       </div>
       <article
-        className="space-y-5 text-justify"
+        className="space-y-5 text-justify tracking-wide"
         dangerouslySetInnerHTML={{ __html: blog.contentHtml }}
       />
     </div>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,7 @@
 export const shortenDate = (date: Date) =>
   date.toLocaleDateString("en-US", {
     year: "2-digit",
-    month: "2-digit",
+    month: "short",
     day: "2-digit",
   });
 
